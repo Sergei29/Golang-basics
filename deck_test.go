8e8b001686cdb6ebfcfc4e1518bb5ec9807e7321
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-08-19 15:01:24 +0300

    feat: unit test for save and read cards to from file

--- deck_test.go
+++ deck_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
+
+var _decktesting string = "_decktesting"
 
 func TestNewDeck(t *testing.T) {
 	deck := newDeck()
@@ -19,4 +22,18 @@ func TestNewDeck(t *testing.T) {
 	if deck[51] != "Ace of Clubs" {
 		t.Errorf("Expected to have 'Ace of Clubs' as the last card, but got %v", deck[51])
 	}
+}
+
+func TestDeckSaveToAndReadFromFile(t *testing.T) {
+	os.Remove(_decktesting)
+
+	deck := newDeck()
+	deck.saveToFile(_decktesting)
+	found := newDeckFromFile(_decktesting)
+
+	if len(found) != 52 {
+		t.Errorf("Expected to have 52 cards in retrieved file, but got %v", len(found))
+	}
+
+	os.Remove(_decktesting)
 }
