acc8b096436786855f8f84e8870f7dc6eb7bfcec
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-08-22 19:48:30 +0300

    chore: removed redundant code

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import "fmt"
-
-type ContactInfo struct {
-	email   string
-	zipCode int
-}
-type Person struct {
-	firstName string
-	lastName  string
-	contact   ContactInfo
-}
-
-func (p Person) print() {
-	fmt.Printf("%+v\n", p)
-}
-
-func (pointerToPerson *Person) changeName(newName string) {
-	(*pointerToPerson).firstName = newName
-}
+import . "fmt"
 
 func main() {
-	jim := Person{
-		firstName: "Jim",
-		lastName:  "Hawkins",
-		contact: ContactInfo{
-			email:   "jim.admiral-benbow@gmail.com",
-			zipCode: 12345,
-		},
-	}
-	jim.changeName("Jamezzz")
-	jim.print()
+	Println("Intefaces!")
 }
