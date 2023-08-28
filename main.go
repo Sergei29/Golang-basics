e1ff6fbab18fe51ce5185ced20d8b31fd22d669a
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-08-28 16:07:40 +0300

    chore: removed redundant code

--- main.go
+++ main.go
@@ -1,10 +1,5 @@
 package main
 
-var filename string = "assets/my_cards"
-
 func main() {
-	retrievedDeck := newDeckFromFile(filename)
-	retrievedDeck.print()
-	retrievedDeck.shuffle()
-	retrievedDeck.print()
+
 }
